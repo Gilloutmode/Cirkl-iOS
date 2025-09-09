92c839dce1a1e5ffe5d57baa43c8c8f268d99748
Gilloutmode <165581114+Gilloutmode@users.noreply.github.com>
2025-09-09 16:44:17 +0300

    Initial Commit

---
+++ CirklTests/CirklTests.swift
@@ -0,0 +1,17 @@
+//
+//  CirklTests.swift
+//  CirklTests
+//
+//  Created by Gil Benittah on 09/09/2025.
+//
+
+import Testing
+@testable import Cirkl
+
+struct CirklTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
